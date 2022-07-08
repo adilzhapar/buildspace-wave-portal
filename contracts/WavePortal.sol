47303b601f9633b1cc73e49dea96b5f8204ec2c5
Adil <adilzhapar2002@gmail.com>
2022-07-08 17:01:30 +0600

    first lessons

---
+++ contracts/WavePortal.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract WavePortal {
+    constructor() {
+        console.log("Salem alem, this is smart contract");
+    }
+    uint256 totalWaves;
+
+    function wave() public {
+        totalWaves += 1;
+        console.log("%s has waved!", msg.sender);
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We have %d total waves!", totalWaves);
+        return totalWaves;
+    }
+}
